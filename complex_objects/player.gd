eebf8ab4ac140186ff679c0499197898cdfac021
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-06-16 18:49:38 -0700

    .

--- complex_objects/player.gd
+++ complex_objects/player.gd
@@ -7,11 +7,14 @@ const SPEED: float = 120.0
 
 func _physics_process(_delta: float) -> void:
 	var movement := Vector2.ZERO
+
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	movement = direction * SPEED
+
 	if abs(movement) > Vector2.ZERO:
 		sprite.play("walk_down")
 	else:
 		sprite.play("idle")
+
 	velocity = movement
 	move_and_slide()
